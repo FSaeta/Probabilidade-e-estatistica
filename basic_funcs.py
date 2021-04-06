cbc622f1e81d6314dda054c9ee93c6aa7eb1ff9c
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-04-06 19:43:10 -0300

    Atualiza função pede_numeros de basic_funcs

--- basic_funcs.py
+++ basic_funcs.py
@@ -7,15 +7,14 @@ def pede_numeros():
     """ Solicita os números que serão adicionados
     - Retorna uma lista com esses números após apertar Ctrl+C"""
     numeros = []
-    pede = True
-    while pede:
+    while True:
         try:
             numero = float(input("numero: "))
             numeros.append(numero)
         except ValueError:
             print(f"{R}Número digitado incorretamente{W}")
         except KeyboardInterrupt:
-            pede = False
+            print(f"{G}-- Done !{W}")
             return numeros
 
 if __name__ == '__main__':
